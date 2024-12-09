dd4071c6ae710d02a159e5eaafad33054f337f69
Nareshkumar Rao <nareshkumar.rao@procom.de>
2024-12-09 22:36:16 +0100

    more efficient implementation of part1

--- src/day9.rs
+++ src/day9.rs
@@ -7,47 +7,43 @@ pub fn day9() {
 }
 
 fn part1() -> usize {
-    let mut input = get_input(INPUT);
-    while !is_dense(&input) {
-        shift(&mut input);
+    let (vals, empties) = get_input(INPUT);
+    let moves_required = empties.len()
+        - vals
+            .iter()
+            .rev()
+            .take(empties.len())
+            .filter(|x| matches!(x, Val::Empty))
+            .count();
+
+    let mut elems_to_move = vals
+        .iter()
+        .rev()
+        .filter(|x| !matches!(x, Val::Empty))
+        .take(moves_required);
+
+    let mut vals = vals.clone();
+    for v in vals.iter_mut() {
+        if matches!(v, Val::Empty) {
+            let Some(next) = elems_to_move.next() else {
+                break;
+            };
+            *v = next.clone();
+        }
     }
-    let total: usize = input
+    let total: usize = vals
         .iter()
         .enumerate()
-        .filter_map(|(idx, elem)| {
-            let Val::Val(val) = elem else {
-                return None;
+        .take(vals.len() - empties.len())
+        .map(|(idx, x)| {
+            let Val::Val(val) = x else {
+                panic!();
             };
-            Some(idx * val)
+            idx * *val
         })
         .sum();
-    println!("DAY9 PART1: {total}");
+
     total
-}
-
-fn is_dense(vals: &[Val]) -> bool {
-    !vals.windows(2).any(|chunks| {
-        matches!(chunks.first(), Some(Val::Empty)) && matches!(chunks.get(1), Some(Val::Val(..)))
-    })
-}
-
-fn shift(vals: &mut [Val]) {
-    let mut to_shift = Val::Empty;
-    for x in vals.iter_mut().rev() {
-        if matches!(x, Val::Empty) {
-            continue;
-        }
-        to_shift = x.clone();
-        *x = Val::Empty;
-        break;
-    }
-    for x in vals.iter_mut() {
-        if !matches!(x, Val::Empty) {
-            continue;
-        }
-        *x = to_shift.clone();
-        break;
-    }
 }
 
 fn part2() {
@@ -60,9 +56,9 @@ enum Val {
     Empty,
 }
 
-fn get_input(input: &str) -> Vec<Val> {
+fn get_input(input: &str) -> (Vec<Val>, Vec<usize>) {
     let input = input.trim().chars().collect::<Vec<_>>();
-    input
+    let vals = input
         .chunks(2)
         .enumerate()
         .flat_map(|(idx, chunk)| {
@@ -79,7 +75,17 @@ fn get_input(input: &str) -> Vec<Val> {
 
             result
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+
+    let mut empties = vals
+        .iter()
+        .enumerate()
+        .filter(|(_, val)| matches!(val, Val::Empty))
+        .map(|(idx, _)| idx)
+        .collect::<Vec<_>>();
+    empties.sort();
+
+    (vals, empties)
 }
 
 const INPUT: &str = include_encrypted_string!("inputs/day9.enc");
@@ -91,7 +97,7 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(1928, part1());
+        assert_eq!(6242766523059, part1());
     }
 
     #[test]
